0731315d85fa837ea95834904a5b0c158676b393
eva <evamarcs4@gmail.com>
2026-03-23 23:10:47 +0100

    JavaScript-proyecto

---
+++ Javascript/js/scripts.js
@@ -0,0 +1,11 @@
+/*Pide un nombre por pantalla*/
+var nombre=prompt("Dime tu nombre");
+alert("Bienvenidos a mi página " + nombre); /*+ para concatenar*/
+let x=5;
+let y=6;
+let z = x + y;
+alert("z: "+z);
+let edad = prompt("Dime tu edad");
+if (edad<18) {mensaje="No puedes acceder a esta página";}
+else {mensaje="Bienvenido a esta página";}
+alert(mensaje);
